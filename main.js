04908f5ece7fd97dbef77449777bab2a1ac44636
Strike <strikeihaveadream@gmail.com>
2024-01-22 05:14:43 +0000

    Must use sendAndConfirmRawTransaction

--- main.js
+++ main.js
@@ -1,10 +1,11 @@
 import fetch from "node-fetch";
 import fs from "fs/promises";
-import { Connection, Keypair, VersionedTransaction, PublicKey } from '@solana/web3.js';
+import { Connection, Keypair, VersionedTransaction, PublicKey, sendAndConfirmRawTransaction } from '@solana/web3.js';
 import bs58 from 'bs58';
 import dotenv from 'dotenv';
 import { Wallet } from '@project-serum/anchor';
 import axios from 'axios';
+import { connect } from "http2";
 
 let selectedAddress = "So11111111111111111111111111111111111111112";
 let selectedToken = "SOL";
@@ -15,8 +16,8 @@ let fixedSwapVal = 0.001; //Swap Amount of Sol or Token
 let slipTarget = 5;
 let refreshTime = 5;
 
-// const usdcMintAddress_pub = "EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v";  //USDC mainnet
-const usdcMintAddress_pub = "4vqYQTjmKjxrWGtbL2tVkbAU1EVAz9JwcYtd2VE3PbVU";  //USDC mainnet
+// const usdcMintAddress_pub = "EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v";  //USDC -token- on -mainnet
+const usdcMintAddress_pub = "4vqYQTjmKjxrWGtbL2tVkbAU1EVAz9JwcYtd2VE3PbVU";  //Anita Max Wynn token - token- on mainnet
 // makeSellTransaction - Sell sol,  makeBuyTransaction - buy sol
 
 async function getTokens() {
@@ -37,7 +38,7 @@ const wallet = new Wallet(Keypair.fromSecretKey(bs58.decode(process.env.PRIVATE_
 // Replace with the Solana network endpoint URL
 const connection = new Connection(process.env.RPC_ENDPOINT, 'confirmed', {
     commitment: 'confirmed',
-    timeout: 60000
+    timeout: 10000
 });
 
 
@@ -71,11 +72,15 @@ async function makeBuyTransaction() {
     transaction.sign([wallet.payer]);
     // Execute the transaction
     const rawTransaction = transaction.serialize()
-    const txid = await connection.sendRawTransaction(rawTransaction, {
+    // const txid = await connection.sendRawTransaction(rawTransaction, {
+    //     skipPreflight: true,
+    //     maxRetries: 2
+    // });
+    // await connection.confirmTransaction({ signature: txid });
+    const txid = await sendAndConfirmRawTransaction(connection, rawTransaction, null, {
         skipPreflight: true,
         maxRetries: 2
     });
-    await connection.confirmTransaction({ signature: txid });
     console.log(`https://solscan.io/tx/${txid}`);
 }
 
@@ -109,11 +114,15 @@ async function makeSellTransaction() {
     transaction.sign([wallet.payer]);
     // Execute the transaction
     const rawTransaction = transaction.serialize()
-    const txid = await connection.sendRawTransaction(rawTransaction, {
+    // const txid = await connection.sendRawTransaction(rawTransaction, {
+    //     skipPreflight: true,
+    //     maxRetries: 2
+    // });
+    //  await connection.confirmTransaction({ signature: txid });
+    const txid = await sendAndConfirmRawTransaction(connection, rawTransaction, null, {
         skipPreflight: true,
         maxRetries: 2
     });
-    await connection.confirmTransaction({ signature: txid });
     console.log(`https://solscan.io/tx/${txid}`);
 }
 
